2e494b218ca861bc4b1628f2ac22f33a33df8b89
Hellobobjoe123 <80554837+Hellobobjoe123@users.noreply.github.com>
2021-05-18 12:08:54 -0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,9 @@
-input.onGesture(Gesture.Shake, function () {
-    steps += 1
-})
-function CountSteps () {
-    if (Time < 86400000) {
-        if (steps < 5000) {
-            basic.showString("Move!")
-        }
+let seconds = 0;
+input.onGesture(Gesture.Shake, () => {
+    while (seconds > 0) {
+        basic.showNumber(seconds);
+        basic.pause(1000);
+        seconds -= 1;
     }
-    if (Time == 86400000 && steps < 10000) {
-        basic.showString("better luck next time!")
-    }
-    if (Time != 86400000 && (steps > 5000 || steps == 5000)) {
-        basic.showIcon(IconNames.Heart)
-        basic.pause(100)
-        basic.clearScreen()
-    }
-}
-let steps = 0
-let Time = 0
-Time = 0
-steps = 0
-for (let index = 0; index < 86400000; index++) {
-    basic.pause(1000)
-    Time += 1
-}
-basic.forever(function () {
-    CountSteps()
+   
 })
